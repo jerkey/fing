8968ae0bd0a8b0fccfb83d6e5e4efbbc2e6d2425
jake waters <jerkey@g-cipher.net>
2017-08-10 00:22:14 -0700

    open two serial ports: native and hardware

---
+++ fing.ino
@@ -0,0 +1,22 @@
+void setup() {
+  Serial.begin(57600); // USB virtual comm port, talking to computer
+  while (!Serial) {} // wait for serial port to connect. Needed for native USB port only
+  Serial.println("native USB port reporting for communications");
+  Serial1.begin(57600); // hardware serial port, talking to fingerprint reader
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    byte inByte = Serial.read();
+  }
+  Serial.println(millis());
+  delay(random(1000));
+}
+
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.println("0,0,0");   // send an initial string
+    delay(300);
+  }
+}
